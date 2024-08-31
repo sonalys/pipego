bf515cb7978822abdb93f47fa1f8c67ce7132080
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com>
2024-08-31 09:55:32 +0200

    refactor: use go slices.Chunk instead of self-implementation

--- slices.go
+++ slices.go
@@ -2,6 +2,7 @@ package pp
 
 import (
 	"context"
+	"slices"
 )
 
 // Group takes any amount of steps and return a single step that bounds them all.
@@ -11,34 +12,11 @@ func Group(steps ...StepFunc) StepFunc {
 	}
 }
 
-// Chunk returns an array of elements split into groups the length of size. If array can't be split evenly,
-// the final chunk will be the remaining elements.
-// Play: https://go.dev/play/p/EeKl0AuTehH
-func Chunk[T any](collection []T, size int) [][]T {
-	if size <= 0 {
-		panic("second parameter must be greater than 0")
-	}
-	chunksNum := len(collection) / size
-	if len(collection)%size != 0 {
-		chunksNum += 1
-	}
-	result := make([][]T, 0, chunksNum)
-	for i := 0; i < chunksNum; i++ {
-		last := (i + 1) * size
-		if last > len(collection) {
-			last = len(collection)
-		}
-		result = append(result, collection[i*size:last])
-	}
-	return result
-}
-
 // DivideSliceInSize receives a slice `s` and divide it into groups with `n` elements each,
 // then it uses a step factory to generate steps for each group.
 // `n` must be greater than 0 or it will panic.
-func DivideSliceInSize[T any](
-	s []T, n int, stepFactory func(T) StepFunc) (steps []StepFunc) {
-	for _, chunk := range Chunk(s, n) {
+func DivideSliceInSize[T any](s []T, n int, stepFactory func(T) StepFunc) (steps []StepFunc) {
+	for chunk := range slices.Chunk(s, n) {
 		batch := make([]StepFunc, len(chunk))
 		for _, v := range chunk {
 			batch = append(batch, stepFactory(v))
@@ -68,8 +46,7 @@ func divideSliceInGroups[T any](s []T, n int) [][]T {
 
 // DivideSliceInGroups receives a slice `s` and divide it into `n` groups,
 // then it uses a step factory to generate steps for each group.
-func DivideSliceInGroups[T any](
-	s []T, n int, stepFactory func(T) StepFunc) (steps []StepFunc) {
+func DivideSliceInGroups[T any](s []T, n int, stepFactory func(T) StepFunc) (steps []StepFunc) {
 	for _, chunk := range divideSliceInGroups(s, n) {
 		batch := make([]StepFunc, len(chunk))
 		for _, v := range chunk {
